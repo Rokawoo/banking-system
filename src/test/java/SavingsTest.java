93495a8f371fd57116d38464f877cef0f8c8198e
Augustus Sroka <ays36@drexel.edu>
2023-10-28 18:36:57 -0400

    Assignment 4

---
+++ src/test/java/SavingsTest.java
@@ -0,0 +1,14 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class SavingsTest {
+    @Test
+    // Checking and Savings: when created, its starting balance is $0.
+    public void savings_created_with_0_balance_by_default() {
+        Savings savings = new Savings(9.8);
+        double actual = savings.getBalance();
+
+        assertEquals(0, actual);
+    }
+}
