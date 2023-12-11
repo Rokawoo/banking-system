f780295059b1937166dc93f9e7367097db2e9771
Augustus Sroka <ays36@drexel.edu>
2023-12-11 13:37:26 -0500

    Method `accrueAPR` has a Cognitive Complexity of 11 (exceeds 5 allowed). Consider refactoring.

---
+++ src/main/java/banking/AprUtils.java
@@ -0,0 +1,32 @@
+package banking;
+
+public class AprUtils {
+    public static double calculateTotalInterest(Account account, int monthsPassed) {
+        double apr = account.getApr() / 100;
+        double monthlyRate = apr / 12;
+        double totalInterest = 0.0;
+
+        for (int monthAccrued = 0; monthAccrued < monthsPassed; ++monthAccrued) {
+            totalInterest += (isCD(account) ? calculateCDInterest(account, monthlyRate) : calculateRegularInterest(account, monthlyRate));
+        }
+
+        return totalInterest;
+    }
+
+    private static boolean isCD(Account account) {
+        return "cd".equals(account.getType());
+    }
+
+    private static double calculateCDInterest(Account account, double monthlyRate) {
+        double totalInterest = 0.0;
+        for (int i = 0; i < 4; ++i) {
+            totalInterest += account.getBalance() * monthlyRate;
+        }
+        return totalInterest;
+    }
+
+    private static double calculateRegularInterest(Account account, double monthlyRate) {
+        return account.getBalance() * monthlyRate;
+    }
+}
+
