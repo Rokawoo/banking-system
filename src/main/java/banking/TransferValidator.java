1685309d0028c77639cb266e4f34babd02dc9ba5
Augustus Sroka <ays36@drexel.edu>
2023-12-11 13:50:06 -0500

    Method `validate` in TransferValidator has 28 lines of code (exceeds 25 allowed). Consider refactoring.

--- src/main/java/banking/TransferValidator.java
+++ src/main/java/banking/TransferValidator.java
@@ -7,43 +7,23 @@ class TransferValidator extends CommandValidatorBase {
 
     @Override
     public boolean validate(String[] commandData) {
-        if (commandData.length != 4) {
-            return false;
-        }
+        if (commandData.length != 4) return false;
 
         String fromAccountId = commandData[1];
         String toAccountId = commandData[2];
         String balanceToTransferStr = commandData[3];
 
-        if (!ValidationUtils.isValidInt(fromAccountId) || !ValidationUtils.isValidInt(toAccountId) || !ValidationUtils.isValidFloat(balanceToTransferStr)) {
-            return false;
-        }
-
-        if (!ValidationUtils.isValidAccountId(fromAccountId) || !ValidationUtils.isValidAccountId(toAccountId)) {
-            return false;
-        }
-
-        if (fromAccountId.equals(toAccountId)) {
-            return false;
-        }
-
-        if (!ValidationUtils.accountExists(bank, fromAccountId) || !ValidationUtils.accountExists(bank, toAccountId)) {
-            return false;
-        }
+        if (!ValidationUtils.isValidInt(fromAccountId) || !ValidationUtils.isValidInt(toAccountId) || !ValidationUtils.isValidFloat(balanceToTransferStr)) return false;
+        if (!ValidationUtils.isValidAccountId(fromAccountId) || !ValidationUtils.isValidAccountId(toAccountId) || fromAccountId.equals(toAccountId)) return false;
+        if (!ValidationUtils.accountExists(bank, fromAccountId) || !ValidationUtils.accountExists(bank, toAccountId)) return false;
 
         Account fromAccount = bank.retrieveAccount(fromAccountId);
         Account toAccount = bank.retrieveAccount(toAccountId);
 
-        if (fromAccount == null || toAccount == null || fromAccount.getType().equals("cd") || toAccount.getType().equals("cd")) {
-            return false;
-        }
+        if (fromAccount == null || toAccount == null || fromAccount.getType().equals("cd") || toAccount.getType().equals("cd")) return false;
 
         float amount = Float.parseFloat(balanceToTransferStr);
 
-        if (amount < 0) {
-            return false;
-        }
-
-        return fromAccount.isValidWithdraw(amount, bank.getTime()) && toAccount.isValidDeposit(amount);
+        return amount >= 0 && fromAccount.isValidWithdraw(amount, bank.getTime()) && toAccount.isValidDeposit(amount);
     }
 }
